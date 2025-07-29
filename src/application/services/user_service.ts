9dc97a57f14f3fe551251f6bd67ab3146e559752
Lucas Ferrari <lucasfcorrea2206@gmail.com>
2025-07-29 15:18:10 -0300

    teste e2e user

--- src/application/services/user_service.ts
+++ src/application/services/user_service.ts
@@ -1,5 +1,7 @@
 import { User } from "../../domain/entities/user";
 import { UserRepository } from "../../domain/repositories/user_repository";
+import { CreateUserDTO } from "../dtos/create_user_dto";
+import { v4 as uuidv4 } from "uuid";
 
 export class UserService {
   constructor(
@@ -8,5 +10,12 @@ export class UserService {
 
   async findUserById(id: string): Promise<User | null> {
     return this.userRepository.findById(id)
+  }
+
+  async createUser(dto: CreateUserDTO): Promise<User> {
+    const user = new User(uuidv4(), dto.name)
+
+    await this.userRepository.save(user);
+    return user
   }
 }
---
+++ src/infrastructure/web/user_controller.ts
@@ -0,0 +1,31 @@
+import { UserService } from "../../application/services/user_service";
+import { CreateUserDTO } from "../../application/dtos/create_user_dto";
+import { Request, Response } from "express";
+
+export class UserController {
+  constructor(
+    private readonly userService: UserService
+  ) {}
+
+  async createUser(req: Request, res: Response): Promise<Response> {
+    try {
+      const dto: CreateUserDTO = {
+        name: req.body.name,
+      };
+  
+      const user = await this.userService.createUser(dto)
+  
+      return res.status(201).json({
+        message: "User created successfully",
+        user: {
+          id: user.getId(),
+          name: user.getName()
+        }
+      })
+    } catch (error: any) {
+      return res
+        .status(400)
+        .json({ message: error.message || "An unexpected error occurred" });
+    }
+  }
+}
